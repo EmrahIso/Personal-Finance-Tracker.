e348737bc18b1462df86d7dc483c47c9e7e2bec1
Emrah Isovic <emrahisovic20@gmail.com>
2026-09-21 22:26:47 +0200

    feat: created different types of navigation layouts

--- frontend/src/components/ui/Navigation.tsx
+++ frontend/src/components/ui/Navigation.tsx
@@ -3,9 +3,64 @@ import LogoutButton from './LogoutButton';
 
 type NavigationProps = {
   authenticated?: boolean;
+  dashboard?: boolean;
 };
 
-const Navigation = ({ authenticated = false }: NavigationProps) => {
+const Navigation = ({
+  authenticated = false,
+  dashboard = false,
+}: NavigationProps) => {
+  let navigationRightSide = null;
+
+  if (!authenticated) {
+    navigationRightSide = (
+      <ul className="flex gap-4 items-center">
+        <li>
+          <Link
+            to="/login"
+            className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
+          >
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
+          >
+            Register
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
+  if (authenticated && !dashboard) {
+    navigationRightSide = (
+      <ul className="flex gap-4 items-center">
+        <li>
+          <Link
+            to="/dashboard"
+            className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <LogoutButton asLink={true} />
+        </li>
+      </ul>
+    );
+  }
+
+  if (authenticated && dashboard) {
+    navigationRightSide = (
+      <ul className="flex gap-4 items-center">
+        <h2>Hello</h2>
+      </ul>
+    );
+  }
+
   return (
     <header className="bg-white py-1.5 border-b border-gray-200">
       <div className="container lg:max-w-292.5 mx-auto flex items-center justify-between py-2 ">
@@ -17,43 +72,7 @@ const Navigation = ({ authenticated = false }: NavigationProps) => {
             Monetra
           </Link>
         </div>
-        <nav>
-          {!authenticated && (
-            <ul className="flex gap-4 items-center">
-              <li>
-                <Link
-                  to="/login"
-                  className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/register"
-                  className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
-                >
-                  Register
-                </Link>
-              </li>
-            </ul>
-          )}
-          {authenticated && (
-            <ul className="flex gap-4 items-center">
-              <li>
-                <Link
-                  to="/dashboard"
-                  className="text-gray-700 text-sm tracking-wide underline-offset-3 hover:underline"
-                >
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <LogoutButton asLink={true} />
-              </li>
-            </ul>
-          )}
-        </nav>
+        <nav>{navigationRightSide}</nav>
       </div>
     </header>
   );
